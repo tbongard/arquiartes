e1a656afd3cdba0a02ca8b19c4c8263beeca4fa7
tbongard <thiagobongard@bjgestao.com>
2026-07-08 10:18:43 -0300

    Atualiza site (painel ARQUIARTES): 2 foto(s) + conteudo

--- assets/conteudo.js
+++ assets/conteudo.js
@@ -296,6 +296,11 @@ window.SITE_CONTENT = {
         "titulo": "Suíte âmbar",
         "antes": "assets/img/foto-1783514578034-87247.jpg",
         "depois": "assets/img/foto-1783514578967-21010.jpg"
+      },
+      {
+        "titulo": "Projeto Oásis",
+        "antes": "assets/img/foto-1783516720983-881480.jpg",
+        "depois": "assets/img/foto-1783516721593-67418.jpg"
       }
     ]
   },
